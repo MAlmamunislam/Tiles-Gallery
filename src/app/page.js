489afe48f658ca2abbc9b3bb9d9d5a98fadef3b0
MAlmamunislam <almamun2026islam@gmail.com>
2026-04-29 19:49:41 +0600

    environment set up

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { FadeLoader, MoonLoader } from "react-spinners";
+
+export default function Home() {
+  return (
+    <div >
+      ok this part
+ 
+    
+    </div>
+  );
+}
