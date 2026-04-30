ad87ed2682e03fff71067ebead123dd0ec037fbf
MAlmamunislam <almamun2026islam@gmail.com>
2026-05-01 01:26:10 +0600

    profile page update

--- src/app/(routes)/(privet)/profile/page.jsx
+++ src/app/(routes)/(privet)/profile/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { authClient } from "@/lib/auth-client";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
@@ -12,15 +13,15 @@ const page = () => {
       <div className="flex justify-center items-center py-4  md:py-50">
         <div className="card w-96 bg-base-100 shadow-2xl border border-gray-200">
           <figure className="px-10 pt-10">
-            <img
-              src="https://via.placeholder.com/150"
+            <Image
+              src={user?.image}
               alt="Profile"
               className="rounded-full w-32 h-32 object-cover border-4 border-primary"
             />
           </figure>
           <div className="card-body items-center text-center">
-            <h2 className="card-title text-2xl font-bold">John Doe</h2>
-            <p className="text-gray-500">john.doe@example.com</p>
+            <h2 className="card-title text-2xl font-bold">{user?.name}</h2>
+            <p className="text-gray-500">{user?.email}</p>
 
             <div className="divider w-full"></div>
 
